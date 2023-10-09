1127cb434e6b8e883f05fc3f66564535ab6404fb
Guilherme Duarte <guilhermeduarte.informatica@gmail.com>
2023-10-08 23:21:08 -0300

    Add gráfico de área em JS

--- js/script.js
+++ js/script.js
@@ -132,118 +132,35 @@ var barChart = new ApexCharts(document.querySelector("#bar-chart"), barChartOpti
 barChart.render();
 
 // AREA chart
-var areaChartOptions = {
-	series: [{
-		name: "Purchase Orders",
-		data: [31, 40, 28, 51, 42, 109, 100],
-	}, {
-		name: "Sales Orders",
-		data: [11, 32, 45, 32, 34, 52, 41],
-	}],
-	chart: {
-		type: "area",
-		background: "transparent",
-		height: 350,
-		stacked: false,
-		toolbar: {
-			show: false,
-		},
-	},
-	colors: ["#00ab57", "#d50000"],
-	labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
-	dataLabels: {
-		enabled: false,
-	},
-	fill: {
-		gradient: {
-			opacityFrom: 0.4,
-			opacityTo: 0.1,
-			shadeIntensity: 1,
-			stops: [0, 100],
-			type: "vertical",
-		},
-		type: "gradient",
-	},
-	grid: {
-		borderColor: "#55596e"
-		yaxis: {
-			lines: {
-				show: true,
-			},
-		},
-		xaxis: {
-			lines: {
-				show: true,
-			},
-		},
-	},
-	legend: {
-		labels: {
-			colors: "#f5f7ff"
-		},
-		show: true,
-		position: "top",
-	},
-	markers: {
-		size: 6,
-		strokeColors: "#1b2635",
-		strokeWidth: 3,
-	},
-	stroke: {
-		curve: "smooth",
-	},
-	xaxis: {
-		axisBorder: {
-			color: "#55596e",
-			show: true,
-		},
-		axisTicks: {
-			color: "#55596e",
-			show: true,
-		},
-		labels: {
-			offsetY: 5,
-			style: {
-				colors: "#f5f7ff",
-			},
-		},
-	},
-	yaxis: 
-	[
-		{
-			title: {
-				text: "Purchase Orders",
-				style: {
-					color: "#f5f7ff",
-				},
-			},
-			labels: {
-				style: {
-					colors: ["#f5f7ff"],
-				},
-			},
-		},
-		{
-			opposite: true,
-			title: {
-				text: "Sales Orders",
-				style: {
-					color: "#f5f7ff",
-				},
-			},
-			labels: {
-				style: {
-					colors: ["#f5f7ff"],
-				},
-			},
-		},
-	],
-	tooltip: {
-		shared: true,
-		intersect: false,
-		theme: "dark",
-	}
-};
 
-var areaChart = new ApexCharts(document.querySelector("#area-chart"), areaChartOptions);
-areaChart.render();
+var areaChartOptions = {
+          series: [{
+          name: 'series1',
+          data: [31, 40, 28, 51, 42, 109, 100]
+        }, {
+          name: 'series2',
+          data: [11, 32, 45, 32, 34, 52, 41]
+        }],
+          chart: {
+          height: 350,
+          type: 'area'
+        },
+        dataLabels: {
+          enabled: false
+        },
+        stroke: {
+          curve: 'smooth'
+        },
+        xaxis: {
+          type: 'datetime',
+          categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
+        },
+        tooltip: {
+          x: {
+            format: 'dd/MM/yy HH:mm'
+          },
+        },
+        };
+
+        var areaChart = new ApexCharts(document.querySelector("#area-chart"), areaChartOptions);
+        areaChart.render();
